f95edca7b6a9c27801bcec87af057b884302ceaf
Hebx <lord.noctx@gmail.com>
2020-12-02 03:43:10 +0000

    iterators

--- Syntax/iterators.js
+++ Syntax/iterators.js
@@ -8,3 +8,20 @@ const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
 const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
 is2p2();
 console.log(is2p2.name);
+const addTwo = (num) => num + 2;
+
+const timeFuncRuntime = (funcParameter) => {
+  const t1 = Date.now();
+  funcParameter();
+  const t2 = Date.now();
+  return t2 - t1;
+};
+const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
+const checkConsistentOutput = (func, val) => {
+  const firstTry = func(val);
+  const secondTry = func(val);
+  if (firstTry === secondTry) {
+    return firstTry;
+  }
+  return 'This function returned inconsistent results';
+};

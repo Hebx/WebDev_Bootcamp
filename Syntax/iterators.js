4db8872dd251eea19a1f1f52030485aa94c753af
Hebx <lord.noctx@gmail.com>
2020-12-03 23:55:55 +0000

    iterators

--- Syntax/iterators.js
+++ Syntax/iterators.js
@@ -1,27 +1,57 @@
-const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
-  for (let i = 1; i <= 1000000; i++) {
-    if (2 + 2 != 4) {
-      console.log('Something has gone very wrong :( ');
-    }
-  }
-};
-const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
-is2p2();
-console.log(is2p2.name);
-const addTwo = (num) => num + 2;
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
 
-const timeFuncRuntime = (funcParameter) => {
-  const t1 = Date.now();
-  funcParameter();
-  const t2 = Date.now();
-  return t2 - t1;
-};
-const time2p2 = timeFuncRuntime(checkThatTwoPlusTwoEqualsFourAMillionTimes);
-const checkConsistentOutput = (func, val) => {
-  const firstTry = func(val);
-  const secondTry = func(val);
-  if (firstTry === secondTry) {
-    return firstTry;
-  }
-  return 'This function returned inconsistent results';
-};
+fruits.forEach((fruit) => console.log(`I want to eat a ${fruit}`));
+
+const animals1 = [
+  'Hen',
+  'elephant',
+  'llama',
+  'leopard',
+  'ostrich',
+  'Whale',
+  'octopus',
+  'rabbit',
+  'lion',
+  'dog',
+];
+
+// Create the secretMessage array below
+const secretMessage = animals.map((animal) => animal[0]);
+
+console.log(secretMessage.join(''));
+
+const bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+const smallNumbers1 = bigNumbers.map((numbers) => numbers / 100);
+
+console.log(smallNumbers1);
+
+const randomNumbers = [375, 200, 3.14, 7, 13, 852];
+
+// Call .filter() on randomNumbers below
+const smallNumbers = randomNumbers.filter((number) => number < 250);
+
+const favoriteWords = [
+  'nostalgia',
+  'hyperbole',
+  'fervent',
+  'esoteric',
+  'serene',
+];
+
+// Call .filter() on favoriteWords below
+const longFavoriteWords = favoriteWords.filter((words) => words.length > 7);
+
+const animals = [
+  'hippo',
+  'tiger',
+  'lion',
+  'seal',
+  'cheetah',
+  'monkey',
+  'salamander',
+  'elephant',
+];
+const foundAnimal = animals.findIndex((animal) => animal === 'elephant');
+const startsWithS = animals.findIndex((animal) => animal.charAt(0) === 's');

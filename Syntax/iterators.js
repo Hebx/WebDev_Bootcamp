66ee93a647d4fc296b305af123225f0991ebe91c
Hebx <lord.noctx@gmail.com>
2020-12-04 22:48:25 +0000

    iterators

--- Syntax/iterators.js
+++ Syntax/iterators.js
@@ -55,3 +55,52 @@ const animals = [
 ];
 const foundAnimal = animals.findIndex((animal) => animal === 'elephant');
 const startsWithS = animals.findIndex((animal) => animal.charAt(0) === 's');
+
+const newNumbers = [1, 3, 5, 7];
+const newSum = newNumbers.reduce((accumulator, currentValue) => {
+  console.log('The value of accumulator: ', accumulator);
+  console.log('The value of currentValue: ', currentValue);
+  return accumulator + currentValue;
+}, 10);
+console.log(newSum);
+
+const words = ['unique', 'uncanny', 'pique', 'oxymoron', 'guise'];
+
+// const check = (word) => word.length < 6 ;
+// console.log(words.some(check));
+console.log(words.some((word) => word.length < 6));
+
+const interestingWords = words.filter((word) => word.length > 5);
+
+console.log(interestingWords.every((word) => word.length > 5));
+
+const cities = [
+  'Orlando',
+  'Dubai',
+  'Edinburgh',
+  'Chennai',
+  'Accra',
+  'Denver',
+  'Eskisehir',
+  'Medellin',
+  'Yokohama',
+];
+
+const nums = [1, 50, 75, 200, 350, 525, 1000];
+
+//  Choose a method that will return undefined
+cities.forEach((city) => console.log(`Have you visited ${city}?`));
+
+// Choose a method that will return a new array
+const longCities = cities.filter((city) => city.length > 7);
+
+// Choose a method that will return a single value
+const word = cities.reduce((acc, currVal) => acc + currVal[0], 'C');
+
+console.log(word);
+
+// Choose a method that will return a new array
+const smallerNums = nums.map((num) => num - 5);
+
+// Choose a method that will return a boolean value
+nums.every((num) => num < 0);

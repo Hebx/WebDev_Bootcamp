19202d7cc96a28195a27d1a7780614db4f4582b5
Hebx <lord.noctx@gmail.com>
2020-12-07 21:48:14 +0000

    Practice

--- Syntax/Practice1.js
+++ Syntax/Practice1.js
@@ -17,3 +17,33 @@ const greetAliens = (arr) => {
   }
 };
 greetAliens(aliens);
+//
+const animals = ['panda', 'turtle', 'giraffe', 'hippo', 'sloth', 'human'];
+const convertToBaby = (arr) => {
+  const toBaby = [];
+  for (let i = 0; i < arr.length; i++) {
+    toBaby.push(`${arr[i]}baby`);
+  }
+  return toBaby;
+};
+convertToBaby(animals);
+console.log(convertToBaby(animals));
+//
+const numbers = [5, 3, 9, 30];
+
+const smallestPowerOfTwo = (arr) => {
+  const results = [];
+  // The 'outer' for loop:
+  for (let i = 0; i < arr.length; i++) {
+    number = arr[i];
+
+    // The 'inner' while loop:
+    // We needed to create a new variable!
+    let j = 1;
+    while (j < number) {
+      j *= 2;
+    }
+    results.push(j);
+  }
+  return results;
+};

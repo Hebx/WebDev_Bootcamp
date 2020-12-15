3891f3917c559ef5dca16071ba48ea6f855671ec
Hebx <lord.noctx@gmail.com>
2020-12-15 14:42:36 +0000

    Practices

--- Syntax/Practice1.js
+++ Syntax/Practice1.js
@@ -47,3 +47,98 @@ const smallestPowerOfTwo = (arr) => {
   }
   return results;
 };
+//
+const veggies = ['broccoli', 'spinach', 'cauliflower', 'broccoflower'];
+
+const politelyDecline = (veg) => {
+  console.log(`No ${veg} please. I will have pizza with extra cheese.`);
+};
+const declineEverything = (arr) => {
+  arr.forEach(politelyDecline);
+};
+const acceptEverything = (arr) => {
+  arr.forEach((e) => {
+    console.log(`Ok, I guess I will eat some ${e}.`);
+  });
+};
+//
+const numbers = [2, 7, 9, 171, 52, 33, 14]
+
+const toSquare = num => num * num
+const squareNums = arr => arr.map(toSquare)
+//
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo']
+const shoutGreetings = arr => arr.map(greet => greet.toUpperCase() + '!')
+console.log(shoutGreetings(greetings))
+/*
+// As a function declaration AND using a loop:
+function shoutGreetings(arr) {
+    let shoutArray = []
+    for(let i = 0; i<arr.length; i++){
+        shoutArray.push(arr[i].toUpperCase() + '!')
+    }
+    return shoutArray
+}
+*/
+//
+const years = [1970, 1999, 1951, 1982, 1963, 2011, 2018, 1922]
+const sortYears = arr => arr.sort().reverse()
+console.log(sortYears(years))
+//
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway'];
+
+const justCoolStuff = (arr1, arr2) => arr1.filter(stuff => arr2.includes(stuff))
+
+console.log(justCoolStuff(myStuff, coolStuff))
+//
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+
+const isTheDinnerVegan = arr => arr.every(food => food.source === 'plant');
+
+console.log(isTheDinnerVegan(dinner))
+//
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
+
+const sortSpeciesByTeeth = arr => arr.sort((species1, species2) => species1.numTeeth > species2.numTeeth);
+
+console.log(sortSpeciesByTeeth(speciesArray))
+//
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+const findMyKeys = arr => arr.indexOf('keys');
+
+console.log(findMyKeys(randomStuff))
+//
+const dogFactory = (name , breed, weight) => {
+ return  {
+   name: name,
+   breed: breed,
+   weight: weight,
+   get name() {
+     return this._name;
+   },
+   set name(newName) {
+     this._name = newName;
+   },
+   get breed() {
+     return this._breed;
+   },
+   set breed(newBreed) {
+     this._breed = newBreed;
+   },
+   get weight() {
+     return this._weight;
+   },
+   set weight(newWeight) {
+     this._weight = newWeight;
+   },
+   bark() {
+     return 'ruff! ruff!'
+   },
+   eatTooManyTreats() {
+     this._weight++
+   }
+ }
+}

c0d0dbf15905403e1cf40a10732c8c8472336d13
Hebx <lord.noctx@gmail.com>
2020-12-18 20:05:27 +0000

    clamp inRange words pad had invert findKey drop dropWhile chunk

--- Lodash/_.js
+++ Lodash/_.js
@@ -17,6 +17,72 @@ const _ = {
     const isInRange = start <= number && number < end;
     return isInRange;
   },
+  words(string) {
+    const words = string.split(' ');
+    return words;
+  },
+  pad(string, length) {
+    if (length <= string.length) {
+      return string;
+    }
+    const startPaddingLength = Math.floor((length - string.length) / 2);
+    const endPaddingLength = length - string.length - startPaddingLength;
+    const paddedString =
+      ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
+    return paddedString;
+  },
+  has(object, key) {
+    const hasValue = object[key];
+    if (hasValue !== undefined) {
+      return true;
+    }
+    return false;
+  },
+  invert(object) {
+    let invertedObject = {};
+    for (const key in object) {
+      const originalValue = object[key];
+      invertedObject = { originalValue: key };
+    }
+    return invertedObject;
+  },
+  findKey(object, predicate) {
+    for (const key in object) {
+      const value = object[key];
+      const predicateReturnValue = predicate(value);
+      if (predicateReturnValue === true) {
+        return key;
+      }
+      return undefined;
+    }
+  },
+  drop(array, n) {
+    if (n === undefined) {
+      n = 1;
+    }
+    const droppedArray = array.slice(n, array.length);
+    return droppedArray;
+  },
+  dropWhile(array, predicate) {
+    const dropNumber = array.findIndex(cb);
+    function cb(element, index) {
+      return !predicate(element, index, array);
+    }
+    const droppedArray = this.drop(array, dropNumber);
+    return droppedArray;
+  },
+  chunk(array, size) {
+    if (size === undefined) {
+      size = 1;
+    }
+    const arrayChunks = [];
+    for (let i = 0; i < array.length; i += size) {
+      const arrayChunk = array.slice(i, i + size);
+      arrayChunks.push(arrayChunk);
+    }
+    return arrayChunks;
+  },
 };
+
 // Do not write or modify code below this line.
 module.exports = _;
